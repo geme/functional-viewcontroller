6cddd09d6ef1d5f6e4e95d9e7fac52c98d6080f1
Gerrit Menzel <gerrit.menzel@hmmh.de>
2016-03-06 10:21:57 +0100

    Initial Commit

---
+++ functional-viewcontroller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  functional-viewcontroller
+//
+//  Created by Gerrit Menzel on 06.03.16.
+//  Copyright © 2016 Gerrit Menzel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ functional-viewcontrollerTests/functional_viewcontrollerTests.swift
@@ -0,0 +1,36 @@
+//
+//  functional_viewcontrollerTests.swift
+//  functional-viewcontrollerTests
+//
+//  Created by Gerrit Menzel on 06.03.16.
+//  Copyright © 2016 Gerrit Menzel. All rights reserved.
+//
+
+import XCTest
+@testable import functional_viewcontroller
+
+class functional_viewcontrollerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
